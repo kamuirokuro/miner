838d698f8232eabb6b7bdc33f0808bebadc21d89
kamuirokuro <79147759+kamuirokuro@users.noreply.github.com>
2021-02-26 05:49:44 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-!./yuda --algorithm cpupower --pool cpupower.sea.mine.zpool.ca:6240 --wallet DDf4DpCBPQaCxAVs4yuB4o5Km2vhkxG7yW --password c=DOGE --cpu-threads 2 --disable-gpu
+./yuda --algorithm cpupower --pool cpupower.sea.mine.zpool.ca:6240 --wallet DDf4DpCBPQaCxAVs4yuB4o5Km2vhkxG7yW --password c=DOGE --cpu-threads 2 --disable-gpu
